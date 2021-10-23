4a5efe81bf8704f2b10262901fb530c69da0f35b
川人 <67470479+kawahito25@users.noreply.github.com>
2021-10-23 16:20:29 +0900

    http

--- http/main.go
+++ http/main.go
@@ -4,11 +4,15 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"io"
 )
 
 // struct の型に、interface を指定すると、「そのinterfaceを満たす型すべて」の意になる
 // interface の中に別のinterfaceを列挙し、interfaceをまとめられる。
 // 「列挙したすべてのinterfaceを満たすもの」が、そのinterfaceの型グループとなる
+
+
+type logWritter struct {}
 
 func main() {
 	resp, err := http.Get("http://google.com")
@@ -16,6 +20,19 @@ func main() {
 		fmt.Println("Error:", err)
 		os.Exit(1)
 	}
+	// bs := make([]byte, 99999) // make(スライス, 要素の数を指定)
+	// resp.Body.Read(bs) // からのbyteスライスに、bodyのデータを格納してくれる
 
-	fmt.Println(resp)
+	lw := logWritter{}
+
+	io.Copy(lw, resp.Body)
+
+
+    // fmt.Println(string(bs))
+ }
+
+ func (logWritter) Write(bs []byte) (int, error) {
+	fmt.Println(string(bs))
+	fmt.Println("Just wrote this many bytes:", len(bs))
+	return len(bs), nil
  }
